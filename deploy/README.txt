d949fe20b704164d4b6b0c6b17d66c9f1c554ed8
ChrisJordan1987 <chrisjordan.18@hotmail.co.uk>
2014-11-19 16:07:34 +0000

    added runnable jar

---
+++ deploy/README.txt
@@ -0,0 +1,3 @@
+Ensure you have JAVA_HOME set to at least JDK version 7
+
+java -jar MartianRobot-1.0-SNAPSHOT.jar
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,18 @@
 import main.MarsRobotApplication;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        String results = new MarsRobotApplication().process("/main/input_sample.txt");
+        System.out.println("INPUT");
+        String inputFile = "/main/input_sample.txt";
+        System.out.println(IOUtils.toString(Main.class.getResourceAsStream(inputFile)));
+        String results = new MarsRobotApplication().process(inputFile);
+        System.out.println("____________________________________________");
+        System.out.println("OUTPUT");
         System.out.println(results);
     }
 }
